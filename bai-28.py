b39643abb158c6a024db119454b5b4e4d06c2d65
ngocdang1999 <nguyenngocdangabcd@gmail.com>
2018-03-25 20:48:44 +0700

    BAI TAP PYTHON

--- bai-28.py
+++ bai-28.py
@@ -1,9 +1,14 @@
+def p28(size):
+    return 1 if size == 1 else p28(size-2) + 4*size*size - 6*(size-1)
 
-from sets import Set
 
-s = Set([])
-for a in xrange(2, 101):
-    for b in xrange(2, 101):
-        s.add(a ** b)
-
-print len(s)
+def main(argv):
+    try:
+        size = int(argv[1])
+    except Exception:
+        usage()
+        return 1
+    if size < 1 or size % 2 == 0:
+        usage()
+        return 1
+    print p28(size)
